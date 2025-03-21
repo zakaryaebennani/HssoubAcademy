62acc79a0f941c538fb38b908eb1508697cc04a2
zakaryae <141943244+zakaryaebennani@users.noreply.github.com>
2025-03-21 21:51:33 +0000

    Delete programming fundamentals/JS exercises directory

--- programming fundamentals/JS exercises/check string elements.js
+++
@@ -1,5 +0,0 @@
-let arr=[10, 154,'13','e'];
-for (let i of arr)
-{
-    if (typeof i == "string") console.log(i);
-}
--- programming fundamentals/JS exercises/sum.js
+++
@@ -1,8 +0,0 @@
-let n=prompt("space separated numbers").trim().split(" ");
-n.forEach((a, i, arr)=> arr[i] = parseInt(a));
-s=0
-for (let i of n)
-{
-    s+=i;
-}
-console.log(s);
