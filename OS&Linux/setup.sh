17b9479c8f6e5fbefc4d3797908457ca647e8159
zakaryae <141943244+zakaryaebennani@users.noreply.github.com>
2025-09-07 02:03:36 +0100

    setup.sh

--- OS&Linux/setup.sh
+++ OS&Linux/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e  # Exit if any command fails
 
-# ----------------------------
 # Create users
-# ----------------------------
 sudo useradd -m -s /bin/bash mohamad
 sudo usermod -aG sudo mohamad
 echo "Set password for mohamad:"
@@ -12,21 +10,19 @@ sudo passwd mohamad
 sudo useradd -m -s /bin/bash ahmed
 sudo useradd -m -s /bin/bash lara
 
-# ----------------------------
 # Create group and add users
-# ----------------------------
 sudo addgroup programming
 sudo usermod -aG programming ahmed
 sudo usermod -aG programming lara
 
-# ----------------------------
+
 # Setup projects directory
-# ----------------------------
+
 mkdir -p projects
 echo "Hello From user mohamad" > ./projects/hsoub_projects.txt
 chmod 775 projects
 
-# Move projects to Ahmed's home and set ownership
+# Move projects to Ahmed's home and change the ownership  
 sudo mv ./projects /home/ahmed
 sudo chown -R ahmed:programming /home/ahmed/projects
 
